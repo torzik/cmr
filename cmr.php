03640bf50b3b8fa6bc8eb04663c03717afaf58be
torzik <konstantin.torzik@gmail.com>
2017-05-14 17:18:59 +0300

    starting plugin

--- cmr.php
+++ cmr.php
@@ -1,3 +1,15 @@
 <?php
+
+
 /**
+* Plugin Name : Car  Maintenance Reminder
+* Plugin URI: https://github.com/torzik/cmr
+* Version : 1.0.0
+* Author : Konstantin
+* License: GPL2
 */
+
+//Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
